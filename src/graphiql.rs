b6f8b69c3fee1c40eb184c1bfb51c960df132128
omar <omaraziz.dev@proton.me>
2024-02-03 21:28:46 -0800

    feat: graphiql

---
+++ src/graphiql.rs
@@ -0,0 +1,81 @@
+use axum::{extract::Path, response::Html};
+
+pub async fn graphiql_handler(Path(endpoint): Path<String>) -> Html<String> {
+    let headers =
+        format!(r#"{{ "Content-Type": "application/json", "Accept": "application/json" }}"#);
+    Html(format!(
+        r#"
+        <!--
+        *  Copyright (c) 2021 GraphQL Contributors
+        *  All rights reserved.
+        -->
+        <!doctype html>
+        <html lang="en">
+          <head>
+            <title>GraphiQL</title>
+            <style>
+              body {{
+                margin: 0;
+                width: 100%;
+                height: 100%;
+                overflow: hidden;
+              }}
+              *::-webkit-scrollbar {{
+                height: 0.3rem;
+                width: 0rem;
+              }}
+              *::-webkit-scrollbar-track {{
+                -ms-overflow-style: none;
+                overflow: -moz-scrollbars-none;
+              }}
+              *::-webkit-scrollbar-thumb {{
+                -ms-overflow-style: none;
+                overflow: -moz-scrollbars-none;
+              }}
+              @supports (scrollbar-gutter: stable) {{
+                html {{
+                  overflow-y: auto;
+                  scrollbar-gutter: stable;
+                }}
+              }}
+              #graphiql {{
+                height: 100vh;
+              }}
+            </style>
+            <script crossorigin src="https://unpkg.com/react/umd/react.development.js"></script>
+            <script crossorigin src="https://unpkg.com/react/umd/react.development.js"></script>
+            <script crossorigin src="https://unpkg.com/react-dom/umd/react-dom.development.js"></script>
+            <script src="https://unpkg.com/graphiql/graphiql.min.js" type="application/javascript"></script>
+            <link
+              rel="shortcut icon"
+              href="https://raw.githubusercontent.com/graphql/graphql-playground/main/packages/graphql-playground-electron/static/icons/icon.ico"
+            />
+            <link
+              rel="favicon"
+              href="https://raw.githubusercontent.com/graphql/graphql-playground/main/packages/graphql-playground-electron/static/icons/icon.ico"
+            />
+            <link rel="stylesheet" href="https://unpkg.com/graphiql/graphiql.min.css" />
+            <script src="https://unpkg.com/@graphiql/plugin-explorer/dist/index.umd.js" crossorigin></script>
+            <link rel="stylesheet" href="https://unpkg.com/@graphiql/plugin-explorer/dist/style.css" />
+          </head>
+          <body>
+            <main id="graphiql">Loading...</main>
+            <script>
+              const root = ReactDOM.createRoot(document.querySelector('main#graphiql'))
+              const fetcher = GraphiQL.createFetcher({{
+                url: '{endpoint}',
+                headers: {headers},
+              }})
+              const explorerPlugin = GraphiQLPluginExplorer.explorerPlugin()
+              root.render(
+                React.createElement(GraphiQL, {{
+                  fetcher,
+                  defaultEditorToolsVisibility: true,
+                  plugins: [explorerPlugin],
+                }})
+              )
+            </script>
+          </body>
+        </html>"#,
+    ))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use axum::{
-    extract::Path,
-    http::StatusCode,
-    response::{Html, IntoResponse},
-    routing::{get, MethodRouter},
-    Router,
-};
+mod graphiql;
+mod index;
 
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+use log::{info, warn};
 use tracing_subscriber::layer::SubscriberExt;
 
-use log::{info, warn};
+use graphiql::graphiql_handler;
+use index::index_handler;
+
+pub type CapturedParams = (String, String);
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() {
@@ -17,10 +17,9 @@ async fn main() {
             .unwrap_or_else(|_| "introspect_app=debug".into()),
     );
     env_logger::init();
-
     let app = Router::new()
         .route("/", get(index_handler))
-        .route("/:format/:url", get(introspect_handler));
+        .route("/graphiql/*endpoint", get(graphiql_handler));
     let app = app.fallback(handler_404);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3443")
@@ -32,36 +31,4 @@ async fn main() {
 
 async fn handler_404() -> impl IntoResponse {
     (StatusCode::NOT_FOUND, "nothing to see here")
-}
-
-async fn index_handler() -> Html<&'static str> {
-    Html(
-        "
-        Missing a valid introspection URL path parameter.
-        The introspection URL should be the URL of a GraphQL endpoint.
-        If you want a JSON GraphQL schema, the format is: 
-            https://introspect.lagon.dev/json/<introspection-url>
-
-        If you want an SDL GraphQL schema, the format is: 
-            https://introspect.lagon.dev/sdl/<introspection-url>
-
-        If you want a GraphQL Playground, the format is:
-            https://introspect.lagon.dev/playground/<introspection-url>
-
-        If no format provided, default is JSON.
-        Example URL: https://introspect.lagon.dev/sdl/https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3
-        Source code: https://github.com/o-az/introspect
-        "
-    )
-}
-
-// https://introspect.lagon.dev/sdl/https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3
-// /:format/:url
-async fn introspect_handler(Path((format, url)): Path<(String, String)>) -> String {
-    info!("format: {}", format);
-    info!("url: {}", url);
-
-    let parts = url.split("/").collect::<Vec<&str>>();
-
-    format!("your parts are: {:?}", parts)
 }
